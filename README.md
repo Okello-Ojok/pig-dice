4391e1ccd46a4f30e5670e63c2e798f9bad8f754
Okello-Ojok Emmanuel <emmanuel.alvino.okello06@gmail.com>
2018-07-09 03:34:27 -0700

    add readme file

---
+++ README.md
@@ -0,0 +1,42 @@
+# PIG DICE PROJECT
+#### Project to show the game of pig dice. 9/July/2018
+#### By **Okello-Ojok Emmanuel**
+
+## Description
+
+## Specs
+
+## Specifications.
+1. Two Players enter their names
+    * Example Input: Player one name - okello
+    * Example Output: Player one name okello is displayed.
+
+2. Player rolls the dice, for any score he/she gets, its added to their score sheet
+
+    * Example Input : turn score = 3;
+    * Example Output: Total score  = 3
+
+3. Player rolls as many times as they except if they roll a 1, if its a one, zero is displayed and becomes another player's turn.
+
+    * Example Input : Turn scores = 2,3,4,5,0;
+    * Example Output: Total scores = total scores remain the same.
+
+
+4. If the player decides to hold, their turn score is added to the total score.
+
+    * Input : Player holds at turn score 10;
+    * Output: Turn score 10 will be added to the total score.
+
+
+
+## Setup/Installation Requirements
+* To view this project, follow this link https://okello-ojok.github.io/pig-dice/ on GitHub pages.
+
+## Known Bugs
+No known bugs yet.
+## Technologies Used
+I used HTML, CSS, Javascript and Bootstrap.
+## Support and contact details
+contact me on Email emmanuel.alvino.okello06@gmail.com and Telephone +256781688946.
+### License
+Copyright (c) 2018 Copyright **Okello-Ojok Emmanuel** All Rights Reserved.
--- js/scripts.js
+++ js/scripts.js
@@ -10,23 +10,20 @@ function Player(name, turnScores, totalScores) {
   this.totalScores = totalScores;
 }
 
-function roll() {
-
-
+Player.prototype.roll = function() {
   let dice = Math.floor(Math.random() * 6) + 1;
 
   if (dice === 1) {
-    //return score;
-    sumScore.push(score);
-  } else {
-    //return turnScore = turnScore + dice;
-    sumScore.push(dice);
 
+    sumScore.push(score);
+    return score;
+  } else {
+    sumScore.push(dice);
   }
-  //return sumScore;
   turnScore = sumScore.reduce((a, b) => a + b, 0);
   return turnScore;
-}
+};
+
 
 function hold() {
   return totalScore = totalScore + turnScore;
@@ -34,37 +31,80 @@ function hold() {
 
 
 
-
-
-
-
-
 //UI LOGIC
 $(document).ready(function() {
   $("form#player-1").submit(function(event) {
     event.preventDefault();
+
     let player1 = $("input#name1").val();
+
+
+    let newPlayer1 = new Player(player1, turnScore, totalScore);
+
     $("input#name1").hide();
     $(".btn1").hide();
-    $("label#showName1").text("Player Name: " + player1);
+    $("label#showName1").text("Player Name: " + newPlayer1.name);
     $("#roll, #hold").show();
 
     $("#roll").click(function() {
-      $("button#show").text(roll());
-      
+      //newPlayer1.diceRoll1();
+      $("#hold2").prop('disabled', true)
+      $("#roll2").prop('disabled', true);
+
+      //newPlayer1.roll();
+      $("button#show").text(newPlayer1.roll());
+      //alert(newPlayer1.win());
+
       //console.log(roll());
       console.log(sumScore);
     })
-    $("#hold").click(function () {
+    $("#hold").click(function() {
       $("button#tot").text(hold());
       $("#roll").prop('disabled', true);
       $(this).prop('disabled', true);
+      $("#roll2").prop('disabled', false);
+      $("#hold2").prop('disabled', false);
 
     })
 
 
   })
 
+
+
+  //PLAYER 2
+  $("form#player-2").submit(function(event) {
+    event.preventDefault();
+
+    let player2 = $("input#name2").val();
+
+    let newPlayer2 = new Player(player2, turnScore, totalScore);
+
+
+    $("input#name2").hide();
+    $(".btn2").hide();
+    $("label#showName2").text("Player Name: " + newPlayer2.name);
+    $("#roll2, #hold2").show();
+
+    $("#roll2").click(function() {
+      $("button#show").text(newPlayer2.roll());
+      $("#roll").prop('disabled', true);
+      $("#hold").prop('disabled', true);
+      //console.log(roll());
+      console.log(sumScore);
+    })
+    $("#hold2").click(function() {
+      $("button#tot").text(hold());
+      $("#roll2").prop('disabled', true);
+      $(this).prop('disabled', true);
+      $("#roll").prop('disabled', false);
+      $("#hold").prop('disabled', false);
+      //$("#roll2").prop('disabled', true);
+      //$(this).prop('disabled', true);
+
+    })
+
+  })
 
 
 
